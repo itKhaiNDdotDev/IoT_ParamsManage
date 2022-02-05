9f644582f32a7d99b7cee612f437092d44286984
hahungle2khust <hung2000yt@gmail.com>
2022-02-06 00:09:03 +0700

    update web interface

--- AndroidApp/app/src/main/java/com/hunglh/soict/appui/MainActivity.java
+++ AndroidApp/app/src/main/java/com/hunglh/soict/appui/MainActivity.java
@@ -3,6 +3,12 @@ package com.hunglh.soict.appui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +18,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        
+        TextView username = (TextView) findViewById(R.id.username);
+        TextView password = (TextView) findViewById(R.id.password);
+
+        MaterialButton btnLogin = (MaterialButton) findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (username.getText().toString().equals("hunghehe")
+                        && password.getText().toString().equals("admin")) {
+                    Toast.makeText(MainActivity.this, "LOGIN SUCCESS!", Toast.LENGTH_SHORT).show();
+                }
+                else Toast.makeText(MainActivity.this, "Incorrect info", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
